f7adf1bb1dac7ea368fc1ed97b1de5d31d8096c5
Lawrence Splaver <lsplaver@yahoo.com>
2021-11-16 12:19:11 -0500

    Added and initially coded Sales model.

---
+++ QuarterlySales/Models/Sales.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuarterlySales.Models
+{
+    public class Sales
+    {
+        public int SalesId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a quarter.")]
+        [Range(1, 4, ErrorMessage = "Quarter must be between 1 and 4.")]
+        public int? Quarter { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter an amount.")]
+        public double? Amount { get; set; }
+
+        [Display(Name = "Employee")]
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; }
+    }
+}
