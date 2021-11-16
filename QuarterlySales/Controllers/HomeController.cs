1b68592193b00aa957ee7316b8799957ac6a42ff
Lawrence Splaver <lsplaver@yahoo.com>
2021-11-16 14:47:06 -0500

    Coded HomeController and fixed Home/Index.

--- QuarterlySales/Controllers/HomeController.cs
+++ QuarterlySales/Controllers/HomeController.cs
@@ -3,14 +3,53 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using QuarterlySales.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace QuarterlySales.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public SalesContext context { get; set; }
+
+        public HomeController(SalesContext ctx) => context = ctx;
+
+        [HttpGet]
+        public ViewResult Index(int id)
         {
-            return View();
+            IQueryable<Sales> query = context.Sales
+                .Include(s => s.Employee)
+                .OrderBy(s => s.Employee.LastName)
+                .ThenBy(s => s.Employee.FirstName)
+                .ThenBy(s => s.Year)
+                .ThenBy(s => s.Quarter);
+
+            if (id > 0)
+            {
+                query = query.Where(s => s.EmployeeId == id);
+            }
+
+            SalesListViewModel vm = new SalesListViewModel
+            {
+                Sales = query.ToList(),
+                Employees = context.Employee.OrderBy(e => e.LastName).ThenBy(e => e.FirstName).ToList(),
+                EmployeeId = id
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Index(Employee employee)
+        {
+            if (employee.EmployeeId > 0)
+            {
+                return RedirectToAction("Index", new { id = employee.EmployeeId });
+            }
+            else
+            {
+                return RedirectToAction("Index", new { id = string.Empty });
+            }
         }
     }
 }
