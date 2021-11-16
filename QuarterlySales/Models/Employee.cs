37baba831c2192873dd6ff7a2a681985404dbb40
Lawrence Splaver <lsplaver@yahoo.com>
2021-11-16 12:13:24 -0500

    Added and initially coded Employee model.

---
+++ QuarterlySales/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuarterlySales.Models
+{
+    public class Employee
+    {
+        public int EnployeeId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a first name.")]
+        [Display(Name = "First Name")]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a last name.")]
+        [Display(Name = "Last Name")]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a birth date.")]
+        [Display(Name = "Birth Date")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Please enter a hire date.")]
+        [Display(Name = "Hire Date")]
+        public DateTime? DateOfHire { get; set; }
+
+        public int ManagerId { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
