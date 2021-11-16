408baa5957b5febffc7ff6fc4bf57a5b1a0a5667
Lawrence Splaver <lsplaver@yahoo.com>
2021-11-16 12:42:19 -0500

    Updated Startup for database.

--- QuarterlySales/Startup.cs
+++ QuarterlySales/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using QuarterlySales.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace QuarterlySales
 {
@@ -24,6 +26,9 @@ namespace QuarterlySales
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SalesContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SalesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
