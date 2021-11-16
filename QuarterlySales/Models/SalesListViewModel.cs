7fe58a60d5199423bfe32570a72ca8c5663ed1ba
Lawrence Splaver <lsplaver@yahoo.com>
2021-11-16 14:28:57 -0500

    Added and Initially coded SalesListViewModel.  Added and coded Home/Index.

---
+++ QuarterlySales/Models/SalesListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterlySales.Models
+{
+    public class SalesListViewModel
+    {
+        public List<Employee> Employees { get; set; }
+
+        public List<Sales> Sales { get; set; }
+
+        public int EmployeeId { get; set; }
+    }
+}
