e05084df656f4a20be801e4a4dc6cca4565035af
Lawrence Splaver <lsplaver@yahoo.com>
2021-12-15 19:47:57 -0500

    Added identity services with custom password options to Startup.  Updated Shared/_Layout to display the user, admin, register and login links.  Added and initially coded AccountController.  Added and coded RegisterViewModel and Account/Register.

---
+++ QuarterlySales/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using QuarterlySales.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterlySales.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userMngr, SignInManager<User> signInMngr)
+        {
+            userManager = userMngr;
+            signInManager = signInMngr;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
--- QuarterlySales/Startup.cs
+++ QuarterlySales/Startup.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using QuarterlySales.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace QuarterlySales
 {
@@ -34,6 +35,14 @@ namespace QuarterlySales
 
             services.AddDbContext<SalesContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("SalesContext")));
+
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<SalesContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +65,7 @@ namespace QuarterlySales
 
             app.UseSession();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
